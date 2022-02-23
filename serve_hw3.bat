d3f6d5ddf363236e2f950703c0616ccdadc91675
zw219 <zw219@duke.edu>
2022-02-22 19:27:56 -0500

    .

---
+++ serve_hw3.bat
@@ -0,0 +1,4 @@
+cd C:\Users\vcm\Desktop\homework_3-1
+git pull https://%TESTAPP_GIT_PAT%@github.com/zw219/homework_3-1.git
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
---
+++ server.py
@@ -0,0 +1,5 @@
+# Serve app on a local port via waitress
+from waitress import serve
+import app
+
+serve(app.server, host='localhost', port=3001)
